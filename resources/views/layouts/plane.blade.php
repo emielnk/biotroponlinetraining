358784953704ef6e6652ea372e27105997bc1098
Ayub Eko Prasetyo <ayubblueknight@gmail.com>
2017-07-26 16:10:00 +0700

    tambahan template

--- resources/views/layouts/plane.blade.php
+++ resources/views/layouts/plane.blade.php
@@ -14,9 +14,33 @@
 	<meta content="" name="Tim PKL Biotrop Ilmu Komputer Institut Pertanian Bogor"/>
 
 	<link rel="stylesheet" href="{{ asset("assets/stylesheets/styles.css") }}" />
+
+	<!-- Bootstrap -->
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Datatables -->
+	<link href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
 	@yield('body')
 	<script src="{{ asset("assets/scripts/frontend.js") }}" type="text/javascript"></script>
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Datatable -->
+    <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+
+    <script>
+        $(function()
+				{
+            var table = $("#listtraining").DataTable();
+        });
+  	</script>
+
+
 </body>
 </html>
